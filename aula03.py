205a65dcf8d7736fb9b8389a7ff32ecb5c621709
Najla Guimaraes <najlasabino.guimaraes@gmail.com>
2025-03-10 19:54:55 -0300

    aula 03- Labpython

---
+++ aula03.py
@@ -0,0 +1,18 @@
+
+# Desafio 
+# Criar um programa para criar a Maioridade
+
+# Passo 1: Criar uma variavel
+# Passo 2: Criar uma estrutura de condicao para verificar a idade
+# Passo 3: Exibir adequadamente de acordo com a idade pessoa.
+
+idade = int(input("Digite sua idade"))
+
+if idade >= 18:
+    print ("Você é maior de adulto")
+
+elif idade >= 12:
+    print("Você é um adolescente")
+
+else:
+    print("Você é uma criança")
